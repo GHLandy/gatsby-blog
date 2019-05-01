6841588cdae640dd01306d7dc74bfada4baf7b47
GHLandy <ghlandy@ghlandy.com>
2019-05-01 10:34:11 +0800

    google analytics

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 module.exports = {
   title: '浮梁好茶',
-  description: '前端小菜鸟，当然要有不断折腾勇气和精神，保持一颗积极乐观的心，学习那些不断涌现的新技术。',
+  description:
+    '前端小菜鸟，当然要有不断折腾勇气和精神，保持一颗积极乐观的心，学习那些不断涌现的新技术。',
   author: 'GHLandy',
+  ga: 'UA-129345549-1', // Google analytics
 };
