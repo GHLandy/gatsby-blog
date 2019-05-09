3ecca56a7d34a4cbac13b14751892be5f0502c7f
GHLandy <ghlandy@ghlandy.com>
2019-05-09 10:02:50 +0800

    update query and add posted date

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,7 +15,7 @@ module.exports = {
 
     return graphql(`
       {
-        allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+        allMarkdownRemark {
           edges {
             node {
               frontmatter {
--- src/templates/Post.js
+++ src/templates/Post.js
@@ -6,12 +6,11 @@ import Layout from '../components/Layout'
 import SEO from '../components/SEO'
 
 export const query = graphql`
-  query BlogPostByPath($path: String!) {
+  query($path: String!) {
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       frontmatter {
         title
         date(formatString: "MMMM DD, YYYY")
-        path
       }
       html
     }
@@ -30,7 +29,10 @@ const Post = props => {
         keywords={['gatsby', 'application', 'react']}
       />
       <div className="site-post">
-        <h1 style={{ textAlign: 'center' }}>{post.frontmatter.title}</h1>
+        <h1 className="post-title">{post.frontmatter.title}</h1>
+
+        <div className="post-date">Posted on: {post.frontmatter.date}</div>
+
         <div
           className="post-content"
           dangerouslySetInnerHTML={{ __html: post.html }}
